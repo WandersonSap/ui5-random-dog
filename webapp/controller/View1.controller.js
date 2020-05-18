713a16a1adc259f6e94e1f2ec57b859576e3d6d5
WandersonSap <wandersoncarvalho@gmail.com>
2020-05-18 16:32:58 +0000

    Aplciativo completo

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -8,6 +8,17 @@ sap.ui.define([
 		endpoint: "https://random.dog/woof.json",
 		
 		onInit: function() {
+			if (!String.prototype.endsWith)
+			  String.prototype.endsWith = function(searchStr, Position) {
+			      // This works much better than >= because
+			      // it compensates for NaN:
+			      if (!(Position < this.length))
+			        Position = this.length;
+			      else
+			        Position |= 0; // round position
+			      return this.substr(Position - searchStr.length,
+			                         searchStr.length) === searchStr;
+			  };
 			this.callAPI() ;
 		},
 		
@@ -16,13 +27,41 @@ sap.ui.define([
 		},
 		
 		callAPI: function() {
-			var callback=this._updatePicture.bind(this);
+			var callback=this._updateContainner.bind(this);
 			
 			$.get(this.endpoint,callback) ;
 		},
 		
-		_updatePicture : function(result){
-			this.byId("containner").setSrc(result.url);
-}
+
+		_updateContainner: function(result){
+			var isVideo = result.url.endsWith("mp4") ;
+			
+			if(isVideo){
+				this._showVideo(result.url);
+			}else{
+				this._showImage(result.url);
+			}
+		
+		},
+		
+		_showVideo: function(sUrl){
+			var video = $("video");
+			video.prop("hidden", false);
+		    this.byId("imageContainerId").setVisible(false);		
+			
+			var source=video.find("source") ;
+			source.attr("src", sUrl);
+			video.load();
+		},
+
+		_showImage: function(sUrl){
+			var video = $("video");
+			video.prop("hidden", true);
+			this.byId("imageContainerId")
+			.setVisible(true)
+			. setSrc(sUrl);				
+			
+		},		
+		
 	});
 });
